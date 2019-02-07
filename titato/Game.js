b467c22808fd53041b65c5c61862146da4c873dc
SAM <isamu3636136@gmail.com>
2019-02-07 10:47:33 -0600

    SAM

---
+++ titato/Game.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Board from './board';
+
+class Game extends Component {
+    constructor(){
+        super();
+        this.state = {
+            stepNumber: 0,
+            history: [{
+                
+            }]
+        }
+    }
+}
